48e8faf31ec5e8c537627b679dbe6778416a768e
styanr <chaus.oleh@gmail.com>
2024-05-03 13:26:20 +0300

    refactor artists_cutoff.py script for improved performance and readability

--- scripts/artists_cutoff.py
+++ scripts/artists_cutoff.py
@@ -1,7 +1,5 @@
 import xml.etree.ElementTree as ET
-from time import sleep
 from rich.console import Console
-
 from rich.progress import (
     Progress,
     BarColumn,
@@ -20,46 +18,37 @@ progress = Progress(
 console = Console()
 
 with console.status("[bold green]Processing XML file...") as status:
-    tree = ET.parse(r'D:\DBMusicDataset\discogs_20240301_artists.xml')
-
-
-root = tree.getroot()
-with console.status("[bold green]Creating parent map...") as status:
-    parent_map = dict((c, p) for p in tree.iter() for c in p)
+    context = ET.iterparse(
+        r'D:\\DBMusicDataset\\discogs_20240301_artists.xml', events=("start", "end"))
+    context = iter(context)
 
 with console.status("[bold green]Creating list of artists...") as status:
-    iterator = list(root.iter('artist'))
+    event, root = next(context)
 
 progress.console.log("[bold green]Processing artists...")
-
-
 count = 0
 maxCount = 5000
-
 task = progress.add_task("[red]Processing...", total=maxCount)
 
-new_root = ET.Element("artists")
+output_file = r'D:\\DBMusicDataset\\artists_correct.xml'
+with open(output_file, "w", encoding="utf-8") as output:
+    output.write('<?xml version="1.0" encoding="UTF-8"?>\n<artists>\n')
 
 with progress:
-    for artist in progress.track(iterator, description="Processing artists..."):
+    for event, elem in progress.track(context, description="Processing artists..."):
         if count >= maxCount:
             break
 
-        progress.console.log(f"Processing {artist.find('name').text}")
-        data_quality = artist.find('data_quality')
-
-        if data_quality.text == 'Correct':
-            progress.console.log(
-                f"Adding {artist.find('name').text} to new root")
-            new_root.append(artist)
-
-            count += 1
-            progress.advance(task)
-
+        if event == "end" and elem.tag == "artist":
+            data_quality = elem.find('data_quality')
+            if data_quality is not None and data_quality.text == 'Correct':
+                with open(output_file, "a", encoding="utf-8") as output:
+                    output.write(ET.tostring(elem, encoding="unicode") + "\n")
+                count += 1
+                progress.advance(task)
+            root.clear()
 
 progress.update(task, completed=count)
 
-
-tree = ET.ElementTree(new_root)
-
-tree.write(r'D:\DBMusicDataset\artists_correct.xml')
+with open(output_file, "a", encoding="utf-8") as output:
+    output.write('</artists>')
